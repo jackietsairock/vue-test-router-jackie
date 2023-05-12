e3e24a5e1e7670cbfbe9c3172f94a0d40c90f8da
jackie <jackieTsai@btnet.com.tw>
2023-05-12 14:42:09 +0800

    增加 createWebHashHistory 解構，目的提醒我有這功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue';
 // import Rwd from '../views/Rwd.vue';
 // import Vue from '../views/Vue.vue';
